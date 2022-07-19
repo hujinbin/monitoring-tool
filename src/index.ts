12b06c432d3d31df45696c2b5852a23e746c5cc4
hujinbin <437044048@qq.com>
2022-07-19 18:00:58 +0800

    feat: load事件监听

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,52 @@
-import {getConnection} from './utils/utils'
+import { getConnection } from './utils/utils'
 
 class monitoringTool {
     constructor(config = {}) {
         console.log(config)
         this.init();
     }
-    init(){
+    init() {
         const connection = getConnection();
         console.log(connection)
-        window.addEventListener('online', ()=>this.onStateChange);
-        window.addEventListener('offline', ()=>this.onStateChange);
-        connection.addEventListener('change', ()=>this.onStateChange);
+        window.addEventListener('online', (e) => this.onStateChange(e));
+        window.addEventListener('offline', (e) => this.onStateChange(e));
+        connection.addEventListener('change', (event) => this.onStateChange(event));
+        //当Promise 被 reject 且没有 reject 处理器的时候，会触发 unhandledrejection 事件
+        window.addEventListener("unhandledrejection", (e) => { console.log("unhandledrejection====", e) })
     }
-    onStateChange(){
-        
+    onStateChange(event) {
+        console.log("event===========", event)
+    }
+    load() {
+            setTimeout(() => {
+                const {
+                    fetchStart,
+                    connectStart,
+                    connectEnd,
+                    requestStart,
+                    responseStart,
+                    responseEnd,
+                    domLoading,
+                    domInteractive,
+                    domContentLoadedEventStart,
+                    domContentLoadedEventEnd,
+                    loadEventStart,
+                } = window.performance.timing;
+                console.log(
+                    fetchStart,
+                    connectStart,
+                    connectEnd,
+                    requestStart,
+                    responseStart,
+                    responseEnd,
+                    domLoading,
+                    domInteractive,
+                    domContentLoadedEventStart,
+                    domContentLoadedEventEnd,
+                    loadEventStart
+                )
+            })
+
     }
 }
 
