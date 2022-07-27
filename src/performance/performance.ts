be3cd6dde94d9b4e35f8383c7e1b2a6e2519c017
hujinbin <437044048@qq.com>
2022-07-27 18:16:24 +0800

    feat: 长任务检测和时间转化

--- src/performance/performance.ts
+++ src/performance/performance.ts
@@ -1,16 +1,15 @@
-import { formatSeconds } from '@/utils/utils'
+import { formatSeconds,getLastEvent,getSelector } from '@/utils/utils'
 
 // 页面性能
 export class performance{
     constructor(){
-        console.log("performance 11111111")
+        this.longTask();
     }
     public onReady() {
-        console.log(33333)
         this.pageLoad();
     }
     // 页面加载性能
-    pageLoad(){
+    public pageLoad(){
         setTimeout(()=>{
             const {
                 fetchStart,
@@ -42,15 +41,22 @@ export class performance{
             if(domContentLoadedEventEnd === 0){
                 this.pageLoad();
             }
+            const connectTime= connectEnd - connectStart, //TCP连接耗时
+            ttfbTime= responseStart - requestStart, //ttfb
+            responseTime= responseEnd - responseStart, //Response响应耗时
+            parseDOMTime= loadEventStart - domLoading, //DOM解析渲染耗时
+            domContentLoadedTime=
+              domContentLoadedEventEnd - domContentLoadedEventStart, //DOMContentLoaded事件回调耗时
+            timeToInteractive= domInteractive - fetchStart, //首次可交互时间
+            loadTime=loadEventStart - fetchStart //完整的加载时间
             const experienceTime = {
-                connectTime: connectEnd - connectStart, //TCP连接耗时
-                ttfbTime: responseStart - requestStart, //ttfb
-                responseTime: responseEnd - responseStart, //Response响应耗时
-                parseDOMTime: loadEventStart - domLoading, //DOM解析渲染耗时
-                domContentLoadedTime:
-                  domContentLoadedEventEnd - domContentLoadedEventStart, //DOMContentLoaded事件回调耗时
-                timeToInteractive: domInteractive - fetchStart, //首次可交互时间
-                loadTime: loadEventStart - fetchStart, //完整的加载时间
+                connectTime, //TCP连接耗时
+                ttfbTime, //ttfb
+                responseTime, //Response响应耗时
+                parseDOMTime, //DOM解析渲染耗时
+                domContentLoadedTime, //DOMContentLoaded事件回调耗时
+                timeToInteractive, //首次可交互时间
+                loadTime, //完整的加载时间
             }
             console.log(experienceTime)
             const experienceTimeSting= {
@@ -71,5 +77,28 @@ export class performance{
             const webMMemory = window.performance.memory
             console.log(webMMemory)
         },3000)  
+    }
+    // 交互长任务
+    public longTask(){
+        console.log("longTask===")
+        new PerformanceObserver((list) => {
+            console.log("PerformanceObserver-list==========",list)
+            list.getEntries().forEach((entry) => {
+                console.log("entry=================",entry)
+              if (entry.duration > 100) {
+                let lastEvent = getLastEvent();
+                console.log("lastEvent============",lastEvent)
+                const longTaskData = {
+                    eventType: lastEvent.type,
+                    startTime: entry.startTime, // 开始时间
+                    duration: entry.duration, // 持续时间
+                    selector: lastEvent
+                      ? getSelector(lastEvent.path || lastEvent.target)
+                      : "",
+                }
+                console.log(longTaskData)
+              }
+            });
+        }).observe({ entryTypes: ["longtask"] });
     }
 }
